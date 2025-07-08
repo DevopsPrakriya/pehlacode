e9f443b00622b46eeafb148a77765f371dc73b50
swapnil060791 <saxenaswapnil033@gmail.com>
2025-07-08 17:16:42 +0530

    main jaa rha hu

--- parent/main.tf
+++ parent/main.tf
@@ -62,6 +62,15 @@ module "vnet" {
   virtual_network_name    = "myVNet"
   address_space           = ["10.0.0.0/16"]
 }
+
+module "swapnilvnet" {
+  depends_on              = [module.resource_group]
+  source                  = "../modules/azurerm_virtual_network"
+  resource_group_name     = "sarrg"
+  resource_group_location = "centralindia"
+  virtual_network_name    = "swapnilVNet"
+  address_space           = ["10.0.0.0/16"]
+}
 module "frontendsubnet" {
   depends_on           = [module.vnet]
   source               = "../modules/azurerm_subnet"
